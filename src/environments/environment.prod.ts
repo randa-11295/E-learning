e9b18fcd43d0039b65970a107f9ac320f989dda1
Randa <randa.mohamed1295@gmail.com>
2022-04-11 23:48:54 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  Api : "http://localhost:3000/api",
+  Api : "https://e-learning-node-server.herokuapp.com/api",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  Api : "http://localhost:3000/api",
+  Api : "https://e-learning-node-server.herokuapp.com/api",
 };
 
